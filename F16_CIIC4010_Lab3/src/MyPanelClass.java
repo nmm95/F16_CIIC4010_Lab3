e2cb157756a191be1497b8d691a6a3275b8833a0
4015d9 <4015d9@quenepa.ece.uprm.edu>
2016-08-31 15:10:41 -0400

    added star

--- F16_CIIC4010_Lab3/src/MyPanelClass.java
+++ F16_CIIC4010_Lab3/src/MyPanelClass.java
@@ -73,8 +73,7 @@ public class MyPanelClass extends JPanel {
                         p2.addPoint(x1 + 38, y1 + 83);
                         g.setColor(Color.WHITE);
                         g.drawPolygon(p2);
-                        
-                        
+                                              
                         
                         
                         
